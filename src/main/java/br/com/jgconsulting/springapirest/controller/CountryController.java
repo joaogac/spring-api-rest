4f86f05f4f1cf73d78d5b50fb089722ffca43222
joaogac <joaogac@gmail.com>
2024-07-09 22:45:39 -0300

    Adding new API GET method /search - finding country by name

--- src/main/java/br/com/jgconsulting/springapirest/controller/CountryController.java
+++ src/main/java/br/com/jgconsulting/springapirest/controller/CountryController.java
@@ -4,6 +4,7 @@ import br.com.jgconsulting.springapirest.controller.dto.CountryDTO;
 import br.com.jgconsulting.springapirest.service.CountryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +22,20 @@ public class CountryController {
     @GetMapping()
     public List<CountryDTO> getAll() {
         return service.getAllCountries();
+    }
+
+    @GetMapping("/search")
+    public CountryDTO getByName(@RequestParam String name) {
+        CountryDTO ret = service.getCountryByName(name);
+        if ( "Brazil".equalsIgnoreCase( name ) ) {
+            ret.setName("Brasil");
+        }
+        else if ( "USA".equalsIgnoreCase( name ) ) {
+            ret.setName("Estados Unidos");
+        }
+        else if ( "Canada".equalsIgnoreCase( name ) ) {
+            ret.setName("Canadá");
+        }
+        return ret;
     }
 }
